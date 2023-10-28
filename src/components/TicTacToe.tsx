c51821559af629799ebd9a13304671b88129e1a4
tindpj <106011455+Tondejphajin@users.noreply.github.com>
2023-10-28 16:05:35 +0700

    add console.log(indx) to each tile

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import Board from "./Board";
 
+const PLAYER_X = "X";
+const PLAYER_Y = "Y";
+
 export default function TicTocToe() {
   const [tiles, setTiles] = useState(Array(9).fill(null));
+  const [playerTurn, setPlayerTurn] = useState(PLAYER_X);
 
+  const handleClick = (tileIndex: number) => {
+    console.log(tileIndex);
+  };
   return (
     <div>
       <h1>Tic Tac Toe</h1>
-      <Board tiles={tiles}></Board>
+      <Board tiles={tiles} onTileClick={handleClick}></Board>
     </div>
   );
 }
